f526afe4183d7f0a0551916d4651acec5b459107
giroberto <umbertobarros@gmail.com>
2020-02-10 02:23:32 -0300

    Store github users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,129 @@
-import React from "react";
+import React, { Component } from "react";
+import { Keyboard, ActivityIndicator, AsyncStorage } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
-import { Container, Form, Input, SubmitButton } from "./styles";
+import api from "../../services/api";
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Add user"
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText
+} from "./styles";
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newUser: "",
+      users: [],
+      loading: false
+    };
+  }
+
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem("users");
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+    if (prevState.users !== users) {
+      AsyncStorage.setItem("users", JSON.stringify(users));
+    }
+  }
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: "",
+      loading: false
+    });
+
+    Keyboard.dismiss();
+  };
+
+  handleNavigate = user => {
+    const { navigation } = this.props;
+
+    navigation.navigate("User", { user });
+  };
+
+  static navigationOptions = {
+    title: "Usuários"
+  };
+
+  render() {
+    const { users, newUser, loading } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Add user"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <MaterialIcons name="add" size={20} color="#FFF" />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
+                <ProfileButtonText>Show profile</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
         />
-        <SubmitButton>
-          <MaterialIcons name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+      </Container>
+    );
+  }
 }
+
+Main.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func
+  }).isRequired
+};
 
 Main.navigationOptions = {
   title: "Usuários"
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { View, Text } from "react-native";
+import PropTypes from "prop-types";
+import api from "../../services/api";
 
-export default function User() {
-  return (
-    <View>
-      <Text />
-    </View>
-  );
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam("user").name
+  });
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      stars: []
+    };
+  }
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam("user");
+
+    const response = await api.get(`/users/${user.login}/starred`);
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+    return (
+      <View>
+        <Text />
+      </View>
+    );
+  }
 }
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func
+  }).isRequired
+};
